7214b8f331c4a4a1ba843d3b72bfa85e5281330e
Yash Gadhade <yashgadhade@gmail.com>
2022-12-05 21:53:28 +0530

    gas.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
   paths: { tests: "tests" },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 0,
+      },
+    },
+  },
 };
 
 export default config;
