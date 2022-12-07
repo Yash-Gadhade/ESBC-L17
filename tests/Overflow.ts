96c7ea488512de6d6a559f280a5d4bd9aaa98325
Yash Gadhade <yashgadhade@gmail.com>
2022-12-07 15:37:14 +0530

    overflow

--- tests/Overflow.ts
+++ tests/Overflow.ts
@@ -1,17 +1,38 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { OverflowTest } from "../typechain-types";
 
-describe("Basic tests for understanding ERC20", async () => {
-    beforeEach(async () => {
+const SAFE_INCREMENT = 99;
+const UNSAFE_INCREMENT = 199;
 
+if (SAFE_INCREMENT + UNSAFE_INCREMENT <= 2 ** 8)
+  throw new Error("Test not properly configured");
+
+describe("Testing Overflow operations", async () => {
+  let testContract: OverflowTest;
+
+  beforeEach(async () => {
+    const testContractFactory = await ethers.getContractFactory("OverflowTest");
+    testContract = await testContractFactory.deploy();
+    await testContract.deployed();
+    const tx = await testContract.increment(SAFE_INCREMENT);
+    await tx.wait();
+  });
+
+  describe("When incrementing under safe circumstances", async () => {
+    it("increments correctly", async () => {
+      // TODO
     });
-
-    it("should have zero total supply at deployment", async () => {
-
+  });
+  describe("When incrementing to overflow", async () => {
+    it("reverts", async () => {
+      // TODO
     });
-
-    it("triggers the Transfer event with the address of the sender when sending transcation", async function () {
-
+  });
+  describe("When incrementing to overflow within a unchecked block", async () => {
+    it("overflows and increments", async () => {
+      // TODO
     });
-})
+  });
+});
